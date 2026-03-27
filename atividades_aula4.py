3fc632db5a343136d1bc4c5d050da9fab0a1e017
elias-adr <eliasandraderodrigues3@gmail.com>
2026-03-27 15:10:05 -0300

    Add files via upload

---
+++ atividades_aula4.py
@@ -0,0 +1,51 @@
+# Elabore um algoritmo que receba dois números, calcule a multiplicação entre eles e apresente o resultado
+'''num1 = int(input("Qual o primeiro número? "))
+num2 = int(input("Qaul o segundo número? "))
+print(f'O resultado da multiplicação entre {num1} e {num2} é', num1 * num2)'''
+
+
+#Desenvolva um algoritmo que receba cinco números, calcule a média aritmética e apresente o resultado final 
+
+'''num1 = float(input('Qual o primeiro número? '))
+num2 = float(input('Qual o segundo número? '))
+num3 = float(input('Qual o terceiro número? '))
+num4 = float(input('Qaul o quarto número? '))
+num5 = float(input('Qual o quinto número? '))
+media = (num1 + num2 + num3 + num4 + num5) / 5
+print(f'A média entre os 5 números é {media}')'''
+
+
+#Construa um algoritmo que receba o valor de um produto e calcule o preço final considerando um acréscimo de 8% de imposto.
+
+'''produto = float(input("Qual o preço do produto? "))
+acesimo = (produto * 0.08) + produto
+print(f"O produto sem o acresimo de 8% é {produto}, e com o acresimo é {acesimo}")'''
+
+
+#Elabore um algoritmo que receba dois números e apresente o resultado da subtração entre eles
+
+'''num1 = float(input("Qual o primeiro número? "))
+num2 = float(input("Qaul o segundo número? "))
+print(f'O resultado da subtração entre { num1} e {num2}, é {num1-num2}')'''
+
+
+
+#Construa um algoritmo que receba a altura e o peso de uma pessoa e calcule o Índice de Massa corporal (imc)
+'''peso = float(input("Qual seu peso? "))
+alt = float(input("Qual sua altura? "))
+imc = peso / (alt * alt) 
+print(f'O seu IMC é {imc:.2f}')'''
+
+
+
+#Elabore um algoritmo que receba a temperatura em graus Celsius e apresente o valor convertido para garus em fahrenheit
+'''temp = int(input("Qaul a temperatura? "))
+fahren = (temp * 9/5) + 32
+print(f'A temperatura em fahrenheit é {fahren}')'''
+
+
+
+#Construa um algoritmo que receba a quantidade de horas trabalhadas por um funcionário e o valor da hora trabalhada, calculando o salário total
+horas_trabalhadas = float(input('Quantas horas você trabalhou? '))
+valor_hora = 7.37
+print(f'Você trabalhou {horas_trabalhadas} e seu salario foi de {valor_hora * horas_trabalhadas:.0f}')
