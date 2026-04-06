430d4d3a0939633fe72c289df76e72f5dde20255
elias-adr <eliasandraderodrigues3@gmail.com>
2026-04-05 21:38:54 -0300

    Delete número_dentro_do_intervalo.py

--- número_dentro_do_intervalo.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Digite um número: "))
-if num <= 10:
-    print(f"O número {num} está do intervalo de 1 a 10.")
-else:
-    print(f'O número {num} não está no intervalo de 1 a 10.')
