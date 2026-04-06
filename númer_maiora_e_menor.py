36b3f0199ecc1bc32372b465de811417ca471c1f
elias-adr <eliasandraderodrigues3@gmail.com>
2026-04-05 21:38:33 -0300

    Delete númer_maiora_e_menor.py

--- númer_maiora_e_menor.py
+++
@@ -1,6 +0,0 @@
-num1 = float(input("Digite um númer: "))
-num2 = float(input("Digite outri número: "))
-if num1 > num2:
-    print(f"O número {num1} é maior.")
-else:
-    print(f"O número {num2} é maior.")
